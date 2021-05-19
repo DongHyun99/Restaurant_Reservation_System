de589a7ff77f5851bf1079cfaa3b54130da7227c
donghyun han <mpolio2@kyonggi.ac.kr>
2021-05-19 15:41:22 +0900

    edit registerActivity

--- app/src/main/java/com/restaurant_reservation_system/controllers/RegisterActivity.java
+++ app/src/main/java/com/restaurant_reservation_system/controllers/RegisterActivity.java
@@ -1,4 +1,18 @@
 package com.restaurant_reservation_system.controllers;
 
-public class RegisterActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import androidx.appcompat.app.AppCompatActivity;
+import com.restaurant_reservation_system.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_register);
+        final View myLayout = findViewById(R.id.register);
+    }
 }
