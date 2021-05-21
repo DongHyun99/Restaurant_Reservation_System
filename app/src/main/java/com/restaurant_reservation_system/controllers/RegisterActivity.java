2eb1eef05293489ccd6476403e9e2bc0e41cf976
민인아 <min_ina@naver.com>
2021-05-21 15:05:19 +0900

    registerRequest클래스 작성, registerActivity 클래스 수정하여 회원가입 데이터베이스 연결

--- app/src/main/java/com/restaurant_reservation_system/controllers/RegisterActivity.java
+++ app/src/main/java/com/restaurant_reservation_system/controllers/RegisterActivity.java
@@ -4,11 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.restaurant_reservation_system.R;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 public class RegisterActivity extends AppCompatActivity {
+    private EditText txtFirstName, txtEmail, txtPhone, txtPassword;
+    private Button btnRegister;
+    private AlertDialog dialog;
+    private boolean isvalid = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +30,6 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         TextView linkLogin = (TextView) findViewById(R.id.linkLogin);
-
         linkLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +37,54 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        txtFirstName= (EditText) findViewById(R.id.txtFirstName);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtPhone = (EditText) findViewById(R.id.txtPhone);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+
+        //Register 버튼 클릭시 수행행
+       btnRegister=findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // EditText에 현재 입력되어있는 값을 get(가져온다)해온다.
+
+                String id = txtEmail.getText().toString();
+                String pw = txtPassword.getText().toString();
+                String NAME = txtFirstName.getText().toString();
+                String phoneNumber = txtPhone.getText().toString();
+                 Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) { // 회원등록에 성공한 경우
+                                Toast.makeText(getApplicationContext(),"회원 등록에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            } else { // 회원등록에 실패한 경우
+                                Toast.makeText(getApplicationContext(),"회원 등록에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+                // 서버로 Volley를 이용해서 요청을 함.
+                RegisterRequest registerRequest = new RegisterRequest(id,pw,NAME,phoneNumber, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+
+
+            }
+        });
 
     }
-}
+
+    }
+
+
+
---
+++ app/src/main/java/com/restaurant_reservation_system/controllers/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.restaurant_reservation_system.controllers;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    // 서버 URL 설정 ( PHP 파일 연동 )
+    final static private String URL = "http://210.100.228.111/edit_user_inform.php";
+    private Map<String, String> map;
+
+
+    public RegisterRequest(String id, String pw, String NAME, String phoneNumber, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("id",id);
+        map.put("pw", pw);
+        map.put("NAME", NAME);
+        map.put("phoneNumber", phoneNumber);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
