fe67ef436a439c8929a482c3e9c62fa388cadcac
donghyun han <mpolio2@kyonggi.ac.kr>
2021-05-22 09:57:01 +0900

    add timetableview

--- app/src/main/java/com/restaurant_reservation_system/controllers/MainActivity.java
+++ app/src/main/java/com/restaurant_reservation_system/controllers/MainActivity.java
@@ -1,7 +1,22 @@
 package com.restaurant_reservation_system.controllers;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import androidx.appcompat.app.AppCompatActivity;
+import com.restaurant_reservation_system.R;
+import com.github.tlaabs.timetableview.Time;
+import com.github.tlaabs.timetableview.TimetableView;
+import com.github.tlaabs.timetableview.Schedule;
+
 
 public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        final View myLayout = findViewById(R.id.main);
+    }
 
 }
